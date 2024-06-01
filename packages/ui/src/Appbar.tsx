6068afe10c35cbec412c2b507e9695aaa9267306
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-01 18:43:08 +0530

    Change website name

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -4,7 +4,6 @@ interface AppbarProps {
     user?: {
         name?: string | null;
     },
-    // TODO: can u figure out what the type should be here?
     onSignin: any,
     onSignout: any
 }
@@ -15,8 +14,8 @@ export const Appbar = ({
     onSignout
 }: AppbarProps) => {
     return <div className="flex justify-between border-b border-slate-300 px-4">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
+        <div className="text-lg flex flex-col justify-center font-bold">
+            QuickPay
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
