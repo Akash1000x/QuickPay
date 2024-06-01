62c07b415f7ae3ba6c106a5df8709eb31551e78f
Akash kumar kumawat <kumawatap63@gmail.com>
2024-06-02 00:27:56 +0530

    show user name on appbar

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -14,7 +14,12 @@ export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
       <div className="text-lg flex flex-col justify-center font-bold">
         QuickPay
       </div>
-      <div className="flex flex-col justify-center pt-2">
+      <div className="flex justify-center pt-2">
+        {user?.name && (
+          <div className="flex justify-center items-center capitalize font-bold rounded-lg text-xl px-5 py-2.5 me-2 mb-2 h-10 mr-2">
+            {user?.name}
+          </div>
+        )}
         <Button onClick={user ? onSignout : onSignin}>
           {user ? "Logout" : "Login"}
         </Button>
