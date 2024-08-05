ad6c3611d63c8c2a4dbf5db16e4ee47cff62113b
Akash kumar kumawat <kumawatap63@gmail.com>
2024-08-05 10:13:04 +0530

    fix: button onClick optional

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 export const Button = ({ onClick, children }: ButtonProps) => {
