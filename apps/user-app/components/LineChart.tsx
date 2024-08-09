0c76345ecd38df00c083213c542f26cb6ee95fae
Akash kumar kumawat <kumawatap63@gmail.com>
2024-08-10 01:05:56 +0530

    update lineChart according to recharts

--- apps/user-app/components/LineChart.tsx
+++ apps/user-app/components/LineChart.tsx
@@ -1,93 +1,152 @@
 "use client";
-
-import React from "react";
-// import { curveLinear } from "@visx/curve";
-import { Group } from "@visx/group";
-import { LinePath } from "@visx/shape";
-import { scaleTime, scaleLinear } from "@visx/scale";
-
+import * as React from "react";
+import { Card } from "@repo/ui/card";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "./chart";
 import { P2pTransferTypes } from "../app/(dashboard)/p2p/page";
-import { AxisLeft, AxisBottom } from "@visx/axis";
 
-export type CurveProps = {
-  width: number;
-  height: number;
-  sentTransfers: P2pTransferTypes[] | undefined;
-  receivedTransfers: P2pTransferTypes[] | undefined;
-};
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#ff0000",
+  },
+} satisfies ChartConfig;
+const chartConfig2 = {
+  desktop: {
+    label: "Desktop",
+    color: "#1500ff",
+  },
+} satisfies ChartConfig;
 
-const LineChart = ({
-  width = 800,
-  height = 400,
+const monthNames = [
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export default function Component({
   sentTransfers,
   receivedTransfers,
-}: CurveProps) => {
-  if (!sentTransfers && !receivedTransfers) {
-    return <div>Loading...</div>;
-  }
+}: {
+  sentTransfers: P2pTransferTypes[] | undefined;
+  receivedTransfers: P2pTransferTypes[] | undefined;
+}) {
+  const [sentChartData, setSentAmoutChartData] = React.useState<any>([]);
+  const [receivedAmountChartData, setReceivedAmoutChartData] =
+    React.useState<any>([]);
 
-  const sentTransfersData = sentTransfers?.map((d) => {
-    return {
-      time: d.timestamp,
-      amount: d.amount,
-    };
-  });
-
-  const receivedTransfersData = receivedTransfers?.map((d) => {
-    return {
-      time: d.timestamp,
-      amount: d.amount,
-    };
-  });
-
-  const amountScale = scaleLinear<number>({
-    domain: [
-      Math.min(...(sentTransfers ?? []).map((d) => d.amount)),
-      Math.max(...(sentTransfers ?? []).map((d) => d.amount)),
-    ],
-    range: [400, 0],
-  });
-
-  const timeScale = scaleTime<number>({
-    domain: [
-      Math.min(...(sentTransfers ?? []).map((d) => d.timestamp)),
-      Math.max(...(sentTransfers ?? []).map((d) => d.timestamp)),
-    ],
-    range: [0, 600],
-  });
-
+  React.useEffect(() => {
+    if (sentTransfers && sentTransfers.length) {
+      setSentAmoutChartData(
+        sentTransfers.map((data) => {
+          return {
+            time: new Date(data.timestamp).toLocaleDateString(),
+            Amount: data.amount,
+          };
+        }),
+      );
+    }
+  }, []);
+  React.useEffect(() => {
+    if (receivedTransfers && receivedTransfers.length) {
+      setReceivedAmoutChartData(
+        receivedTransfers.map((data) => {
+          return {
+            time: new Date(data.timestamp).toLocaleDateString(),
+            Amount: data.amount,
+          };
+        }),
+      );
+    }
+  }, []);
   return (
-    <div>
-      <svg width={width} height={height}>
-        <rect
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          fill="#f3f3f3"
-          rx={10}
-        />
-        <Group left={50} top={50}>
-          <AxisBottom scale={timeScale} top={400} numTicks={10} />
-          <AxisLeft scale={amountScale} left={0} />
-          <LinePath
-            data={sentTransfersData}
-            x={(d) => timeScale(d.time) ?? 0}
-            y={(d) => amountScale(d.amount) ?? 0}
-            stroke="red"
-            strokeWidth={2}
-          />
-          <LinePath
-            data={receivedTransfersData}
-            x={(d) => timeScale(d.time) ?? 0}
-            y={(d) => amountScale(d.amount) ?? 0}
-            stroke="blue"
-            strokeWidth={2}
-          />
-        </Group>
-      </svg>
+    <div className="space-y-6">
+      <Card title={"Received"} clasName="border-t-0">
+        <ChartContainer config={chartConfig} className="min-h-[220px] w-full">
+          <AreaChart
+            accessibilityLayer
+            data={receivedAmountChartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="time"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) =>
+                String(monthNames[parseInt(value.slice(3, 5))]).slice(0, 3)
+              }
+            />
+            <ChartTooltip
+              cursor={true}
+              content={
+                <ChartTooltipContent label={"Amount: "} indicator="line" />
+              }
+              label={"Amount: "}
+            />
+            <Area
+              dataKey="Amount"
+              type="monotone"
+              fill={chartConfig2.desktop.color}
+              fillOpacity={0.4}
+              stroke={chartConfig2.desktop.color}
+            />
+          </AreaChart>
+        </ChartContainer>
+      </Card>
+      <Card title="Sent">
+        <ChartContainer config={chartConfig} className="min-h-[220px] w-full">
+          <AreaChart
+            accessibilityLayer
+            data={sentChartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="time"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) =>
+                String(monthNames[parseInt(value.slice(3, 5))]).slice(0, 3)
+              }
+            />
+            <ChartTooltip
+              cursor={true}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Area
+              dataKey="Amount"
+              type="monotone"
+              fill={chartConfig.desktop.color}
+              fillOpacity={0.4}
+              stroke={chartConfig.desktop.color}
+            />
+          </AreaChart>
+        </ChartContainer>
+      </Card>
     </div>
   );
-};
-
-export default LineChart;
+}
