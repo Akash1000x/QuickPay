0dcccb21fcaafc2e6b21e58a674cd0e52c84f026
Akash kumar kumawat <kumawatap63@gmail.com>
2024-08-10 01:05:27 +0530

    update card

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,12 +1,14 @@
 export function Card({
   title,
   children,
+  clasName,
 }: {
-  title: string;
+  title?: string;
   children?: React.ReactNode;
+  clasName?: string;
 }): JSX.Element {
   return (
-    <div className="border p-6 bg-white rounded-xl bg-[#ededed]">
+    <div className={`border p-6 bg-white rounded-xl bg-[#ededed] ${clasName}`}>
       <h1 className="text-xl border-b pb-2">{title}</h1>
       <p>{children}</p>
     </div>
